2544232b71a1837729c49ea1364217a1975fde7f
Rubi Yehezkel <rubke08@gmail.com>
2024-07-21 22:31:44 +0300

    adding new order features

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -12,6 +12,18 @@ const MobileNavLinks = () => {
       >
         User Profile
       </Link>
+      <Link
+        to="/order-status"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        Order Status
+      </Link>
+      <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        Manage Restaurant
+      </Link>
       <Button
         onClick={() => logout()}
         className="flex items-center px-3 font-bold hover:bg-gray-500"
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,8 +1,11 @@
 import {
   useCreateMyRestaurant,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import { userGetMyRestaurant } from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
@@ -11,14 +14,32 @@ const ManageRestaurantPage = () => {
   const { restaurant } = userGetMyRestaurant();
   const { updateRestaurant, isLoading: isLoadingUpdate } =
     useUpdateMyRestaurant();
+  const { orders } = useGetMyRestaurantOrders();
   const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isLoadingCreate || isLoadingUpdate}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 pg-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isLoadingCreate || isLoadingUpdate}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
